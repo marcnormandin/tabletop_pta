483d3cd52ae8d060b33c882a78cc0b5b53f238dc
Marc Normandin <marc.normandin@utrgv.edu>
2015-11-28 22:28:12 -0600

    Attempted to add app bar tp preferneces activity, but it doesnt show up.

--- app/src/main/java/edu/utrgv/cgwa/metrec/PreferencesActivity.java
+++ app/src/main/java/edu/utrgv/cgwa/metrec/PreferencesActivity.java
@@ -1,15 +1,23 @@
 package edu.utrgv.cgwa.metrec;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 
-public class PreferencesActivity extends Activity {
+public class PreferencesActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // This is safe to call upon first application launch,
         // and subsequent launches. The default values will only
